7b11df426dbf9162c48f4e568307374c8ade1859
BeerSeeker <christopher.aupet@epitech.eu>
2015-10-24 16:05:44 +0200

    js: app/checkroute disable navigation for unregister users

--- www/js/app.js
+++ www/js/app.js
@@ -21,10 +21,21 @@
         .otherwise({redirectTo: '/'});
     }]);
 
-    app.controller('checkRouteController', ['$location', function ($location) {
+    app.controller('checkRouteController', ['$location', function ($location, $scope) {
+        var user;
+        if (sessionStorage.getItem('user'))
+            user = JSON.parse(sessionStorage.getItem('user'));
+
         if ($location.path() === "/") {
             localStorage.clear();
         }
+        else if (!user.data.id) {
+            $scope.$on('$locationChangeStart', function (event) {
+                event.preventDefault();
+            });
+        }
+
+
     }]);
 
     app.directive('signup', [function () {
@@ -61,27 +72,29 @@
         return {
             restrict: 'A',
             controller: function ($scope, $location, postReq, flash, dataCheck) {
-                var dataSend = {"username": null, "password": null},
-                    check = dataCheck([$scope.si_username, $scope.si_password]);
+                this.action = function () {
+                    var dataSend = {"username": null, "password": null},
+                        check = dataCheck([$scope.si_username, $scope.si_password]);
 
-                if (!check) {
-                    flash('Invalid informations', "warning");
-                    return;
-                }
-
-                dataSend.username = $scope.si_username;
-                dataSend.password = $scope.si_password;
-
-                postReq('http://localhost/hackaton/API/signin.php', dataSend, function (data) {
-                    console.log(data);
-                    if (data.error) {
-                        flash(data.error_msg, "warning");
+                    if (!check) {
+                        flash('Invalid informations', "warning");
                         return;
                     }
 
-                    sessionStorage.setItem('user', JSON.stringify(data.data));
-                    $location.path('/home');
-                });
+                    dataSend.username = $scope.si_username;
+                    dataSend.password = $scope.si_password;
+
+                    postReq('http://localhost/hackaton/API/signin.php', dataSend, function (data) {
+                        console.log(data);
+                        if (data.error) {
+                            flash(data.error_msg, "warning");
+                            return;
+                        }
+
+                        sessionStorage.setItem('user', JSON.stringify(data.data));
+                        $location.path('/home');
+                    });
+                };
             },
             controllerAs: "signinCtrl"
         };
